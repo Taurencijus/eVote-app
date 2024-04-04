393f95f5964a12cb96bc7f6d6c852d9dd848afc7
Taurencijus <taurasmonkus@gmail.com>
2024-04-04 09:41:35 +0300

    Initial commit

---
+++ src/main/java/javau9/ca/evote/EVoteApplication.java
@@ -0,0 +1,13 @@
+package javau9.ca.evote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EVoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EVoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/javau9/ca/evote/EVoteApplicationTests.java
@@ -0,0 +1,13 @@
+package javau9.ca.evote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EVoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
